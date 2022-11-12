7ac5773949261785c8a374768151e9f88dfbbc81
Oliver Christensen <orc13a@live.dk>
2022-11-12 17:45:41 +0100

    maps not working

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.runtime.onMessage.addListener(function(request) {
+/*chrome.runtime.onMessage.addListener(function(request) {
     chrome.tts.speak(request.toSay, { 
-        rate: 0.9,
+        rate: 0.8,
         lang: 'da-DK',
         voiceName: 'Microsoft Helle - Danish (Denmark)',
         onEvent: function(e) {}
@@ -8,4 +8,4 @@ chrome.runtime.onMessage.addListener(function(request) {
         console.log('Got message');
         return true;
     });
-});
+});*/
--- content-script.js
+++ content-script.js
@@ -12,7 +12,7 @@ let observer = new MutationObserver(mutations => {
                     const messages = addedNode;
                     const newestPush = messages.children[0];
                     focusNewestPush(newestPush);
-                    ttsNewestPush(newestPush);
+                    ttsNewestPush(newestPush, 1000);
                 }
             }
         }
@@ -34,28 +34,31 @@ let observer = new MutationObserver(mutations => {
 
 function focusNewestPush(pushElt) {
     pushElt.click();
+    embedMaps();
 }
 
-function ttsNewestPush(pushElt) {
+function ttsNewestPush(pushElt, delay) {
     const newestPushContent = pushElt.children[2].children[1];
-    const newestPushMsg = newestPushContent.innerHTML.toString();
+    const newestPushMsg = newestPushContent.innerHTML.toString().split('RSE_')[0].replace('FUH', 'færdselsuheld').replace('Min.', 'mindre').replace('MTV', 'motervejen...');
 
-    chrome.runtime.sendMessage({
-        toSay: newestPushMsg
-    }, function() {
-        msgBefore = newestPushMsg;
-
-        // if (newestPushHerf !== undefined) {
-        //     if (openedTab !== null) {
-        //         openedTab.close();
-        //     }
-        //     
-        //     const newestPushCord = newestPushHerf.href.split('=')[1].replace('%22', '');
-        //     openMaps(newestPushCord);
-        // }
-
-        return true;
-    });
+    setTimeout(() => {
+        chrome.runtime.sendMessage({
+            toSay: newestPushMsg
+        }, function() {
+            msgBefore = newestPushMsg;
+    
+            // if (newestPushHerf !== undefined) {
+            //     if (openedTab !== null) {
+            //         openedTab.close();
+            //     }
+            //     
+            //     const newestPushCord = newestPushHerf.href.split('=')[1].replace('%22', '');
+            //     openMaps(newestPushCord);
+            // }
+    
+            return true;
+        });
+    }, delay);
 }
 
 
@@ -116,4 +119,31 @@ function openMaps(cord) {
     let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=1000,height=800,left=50,top=500`;
 
     openedTab = open(`https://www.google.dk/maps/dir/Karen+Schacks+Vej+42,+2800+Kongens+Lyngby/${cord}/@55.7337835,12.3728096,12z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x46524e7776ee07f3:0x40b47e6cd531d689!2m2!1d12.5276465!2d55.7756502!1m0`, 'test', params);
+}
+
+function embedMaps() {
+    const bigMessageContainer = document.getElementById('big_message');
+
+    try {
+        const mapsContainer = document.getElementById('p_plus-embeded-maps');
+        mapsContainer.remove();
+    } catch (error) {
+        console.error(error);
+    }
+
+    const mapsContainer = document.createElement('div');
+    mapsContainer.setAttribute('id', 'p_plus-embeded-maps');
+    mapsContainer.setAttribute('style', 'width: 100%; height: 375px;');
+    bigMessageContainer.appendChild(mapsContainer);
+
+    const iframeMaps = document.createElement('iframe');
+    iframeMaps.setAttribute('width', '375px');
+    iframeMaps.setAttribute('height', '375px');
+    iframeMaps.setAttribute('style', 'border:0px;');
+    iframeMaps.setAttribute('loading', 'lazy');
+    iframeMaps.setAttribute('allowfullscreen', '');
+    iframeMaps.setAttribute('referrerpolicy', 'no-referrer-when-downgrade');
+    iframeMaps.setAttribute('src', 
+    `https://www.google.com/maps/embed/v1/directions?key=AIzaSyAF1E4kdwZVswl1rsHwKUjimojtXi-Bxp4&origin=place_id:ChIJ6WzacndOUkYR0_ozzT-vnyc&destination=Telemark+Norway`);
+    mapsContainer.appendChild(iframeMaps);
 }
