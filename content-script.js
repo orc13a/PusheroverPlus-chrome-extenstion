cb4af19c96be9e1a7860c76f0e22a6a4626380ae
Oliver Christensen <orc13a@live.dk>
2022-12-05 18:52:22 +0100

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,6 +1,121 @@
 console.log(`    ____             __                                       \r\n   \/ __ \\__  _______\/ \/_  ___  _________ _   _____  _____  __ \r\n  \/ \/_\/ \/ \/ \/ \/ ___\/ __ \\\/ _ \\\/ ___\/ __ \\ | \/ \/ _ \\\/ ___\/_\/ \/_\r\n \/ ____\/ \/_\/ (__  ) \/ \/ \/  __\/ \/  \/ \/_\/ \/ |\/ \/  __\/ \/  \/_  __\/\r\n\/_\/    \\__,_\/____\/_\/ \/_\/\\___\/_\/   \\____\/|___\/\\___\/_\/    \/_\/  `);
 // ####################################################################################
 
+let messagesObserver = new MutationObserver(mutations => {
+    for (let mutation of mutations) {
+        for (const addedNode of mutation.addedNodes) {
+            if (addedNode.nodeName === "DIV") {
+                if (addedNode.id === 'messages') {
+                    const messages = addedNode;
+                    const newestPush = messages.children[0];
+                    focusNewestPush(newestPush);
+
+                    setTimeout(() => {
+                        if (isMessagePfTrafik() === true) {
+                            const messageInfo = getFocusPfTrafikMessage();
+                            showGoogleMaps(messageInfo.cors);
+                        } else {
+                            const messageInfo = getFocusMessage();
+                            showGoogleMaps(messageInfo.cors);
+                        }
+                    }, 1000);
+                }
+            }
+        }
+    }
+});
+messagesObserver.observe(document, { childList: true, subtree: true });
+
+const focusNewestPush = (pushElt) => {
+    pushElt.click();
+}
+
+const isMessagePfTrafik = () => {
+    const big_message_title = document.getElementById('big_message_title');
+
+    if (big_message_title.innerText.includes('PF Trafik')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// ########################################
+// Google Maps
+// ########################################
+
+const getFocusPfTrafikMessage = () => {
+    const big_message_message = document.getElementById('big_message_message');
+    const mapsLink = big_message_message.children[big_message_message.childElementCount -1];
+    const cors = mapsLink.getAttribute('href').split('&query=')[1];
+    big_message_message.removeChild(mapsLink);
+
+    return {
+        message: big_message_message.innerText,
+        cors: cors
+    }
+}
+
+const getFocusMessage = () => {
+    const big_message_message = document.getElementById('big_message_message');
+    const mapsLink = big_message_message.children[big_message_message.childElementCount -1];
+    const cors = mapsLink.getAttribute('href').split('maps?q=')[1].replace('"', '');
+
+
+    return {
+        message: big_message_message.innerText,
+        cors: cors
+    }
+}
+
+const showGoogleMaps = (destinationCors) => {
+    const bigMessageContainer = document.getElementById('big_message');
+
+    try {
+        const mapsContainer = document.getElementById('p_plus-embeded-maps');
+        bigMessageContainer.removeChild(mapsContainer);
+    } catch (error) {
+        console.error('Intet map at fjerne...');
+    }
+
+    const mapsContainer = document.createElement('div');
+    mapsContainer.setAttribute('id', 'p_plus-embeded-maps');
+    mapsContainer.setAttribute('style', 'width: 100%; height: 375px;');
+    bigMessageContainer.appendChild(mapsContainer);
+
+    const iframeMaps = document.createElement('iframe');
+    iframeMaps.setAttribute('width', '100%'); // 375
+    iframeMaps.setAttribute('height', '450px'); // 450
+    iframeMaps.setAttribute('style', 'border:0px;');
+    iframeMaps.setAttribute('loading', 'lazy');
+    iframeMaps.setAttribute('allowfullscreen', '');
+    iframeMaps.setAttribute('referrerpolicy', 'no-referrer-when-downgrade');
+    iframeMaps.setAttribute('src', 
+    `https://www.google.com/maps/embed/v1/directions?key=AIzaSyAF1E4kdwZVswl1rsHwKUjimojtXi-Bxp4&origin=place_id:ChIJ6WzacndOUkYR0_ozzT-vnyc&destination=${destinationCors}`);
+    mapsContainer.appendChild(iframeMaps);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
