a79cc444ed4902b9fd06ff2b005c94e3e3eee204
Oliver Christensen <orc13a@live.dk>
2022-12-08 19:55:51 +0100

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -18,7 +18,8 @@ let messagesObserver = new MutationObserver(mutations => {
                         } else {
                             const messageInfo = getFocusMessage();
                             showGoogleMaps(messageInfo.cors);
-                            ttsMessage(messageInfo.message.split('RSE_')[0]);
+                            const newMessage = translateMessage(messageInfo.message);
+                            ttsMessage(newMessage);
                         }
                     }, 1000);
                 }
@@ -30,6 +31,21 @@ messagesObserver.observe(document, { childList: true, subtree: true });
 
 const focusNewestPush = (pushElt) => {
     pushElt.click();
+}
+
+const translateMessage = (message) => {
+    let newMsg = message.split('RSE_')[0];
+
+    try {
+        const ssFirstS = newMsg.split('#SS');
+        const ssSecoundS = ssFirstS[1].split('#');
+
+        newMsg = ssFirstS[0] + '... ' + ssSecoundS[1];
+    } catch (error) {
+        
+    }
+    console.log(newMsg);
+    return newMsg;
 }
 
 const isMessagePfTrafik = () => {
@@ -44,7 +60,7 @@ const isMessagePfTrafik = () => {
 
 const getFocusPfTrafikMessage = () => {
     const big_message_message = document.getElementById('big_message_message');
-    const mapsLink = big_message_message.children[big_message_message.childElementCount -1];
+    const mapsLink = big_message_message.children[big_message_message.childElementCount - 1];
     const cors = mapsLink.getAttribute('href').split('&query=')[1];
     big_message_message.removeChild(mapsLink);
 
@@ -56,8 +72,14 @@ const getFocusPfTrafikMessage = () => {
 
 const getFocusMessage = () => {
     const big_message_message = document.getElementById('big_message_message');
-    const mapsLink = big_message_message.children[big_message_message.childElementCount -1];
-    const cors = mapsLink.getAttribute('href').split('maps?q=')[1].replace('"', '');
+    const mapsLink = big_message_message.children[big_message_message.childElementCount - 1];
+    let cors = undefined;
+
+    try {
+        cors = mapsLink.getAttribute('href').split('maps?q=')[1].replace('"', '')
+    } catch (error) {
+        console.error('Kunne ikke finde nogen koordinater');
+    }
 
 
     return {
@@ -70,42 +92,46 @@ const ttsMessage = (pushMessage) => {
     setTimeout(() => {
         chrome.runtime.sendMessage({
             toSay: pushMessage
-        }, function() {
+        }, function () {
             return true;
         });
     }, 2000);
 }
- 
+
 // ########################################
 // Google Maps
 // ########################################
 
 const showGoogleMaps = (destinationCors) => {
-    const bigMessageContainer = document.getElementById('big_message');
+    if (destinationCors !== null) {
+        const bigMessageContainer = document.getElementById('big_message');
 
-    const checkMapsContainer = document.getElementById('p_plus-embeded-maps');
+        const checkMapsContainer = document.getElementById('p_plus-embeded-maps');
 
-    if (checkMapsContainer !== null) {
-        bigMessageContainer.removeChild(checkMapsContainer);
+        if (checkMapsContainer !== null) {
+            bigMessageContainer.removeChild(checkMapsContainer);
+        }
+
+        const mapsContainer = document.createElement('div');
+        mapsContainer.setAttribute('id', 'p_plus-embeded-maps');
+        mapsContainer.setAttribute('style', 'width: 100%; height: 375px;');
+        bigMessageContainer.appendChild(mapsContainer);
+
+        const iframeMaps = document.createElement('iframe');
+        iframeMaps.setAttribute('width', '100%'); // 375
+        iframeMaps.setAttribute('height', '450px'); // 450
+        iframeMaps.setAttribute('style', 'border:0px;');
+        iframeMaps.setAttribute('loading', 'lazy');
+        iframeMaps.setAttribute('allowfullscreen', '');
+        iframeMaps.setAttribute('referrerpolicy', 'no-referrer-when-downgrade');
+        iframeMaps.setAttribute('src',
+            `https://www.google.com/maps/embed/v1/directions?key=AIzaSyAF1E4kdwZVswl1rsHwKUjimojtXi-Bxp4&origin=place_id:ChIJ6WzacndOUkYR0_ozzT-vnyc&destination=${destinationCors}`);
+        mapsContainer.appendChild(iframeMaps);
+
+        console.log(`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAF1E4kdwZVswl1rsHwKUjimojtXi-Bxp4&origin=place_id:ChIJ6WzacndOUkYR0_ozzT-vnyc&destination=${destinationCors}`);
+    } else {
+        console.error('Kunne ikke vise Google Maps, da der ingen koordinater er.');
     }
-
-    const mapsContainer = document.createElement('div');
-    mapsContainer.setAttribute('id', 'p_plus-embeded-maps');
-    mapsContainer.setAttribute('style', 'width: 100%; height: 375px;');
-    bigMessageContainer.appendChild(mapsContainer);
-
-    const iframeMaps = document.createElement('iframe');
-    iframeMaps.setAttribute('width', '100%'); // 375
-    iframeMaps.setAttribute('height', '450px'); // 450
-    iframeMaps.setAttribute('style', 'border:0px;');
-    iframeMaps.setAttribute('loading', 'lazy');
-    iframeMaps.setAttribute('allowfullscreen', '');
-    iframeMaps.setAttribute('referrerpolicy', 'no-referrer-when-downgrade');
-    iframeMaps.setAttribute('src', 
-    `https://www.google.com/maps/embed/v1/directions?key=AIzaSyAF1E4kdwZVswl1rsHwKUjimojtXi-Bxp4&origin=place_id:ChIJ6WzacndOUkYR0_ozzT-vnyc&destination=${destinationCors}`);
-    mapsContainer.appendChild(iframeMaps);
-
-    console.log(`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAF1E4kdwZVswl1rsHwKUjimojtXi-Bxp4&origin=place_id:ChIJ6WzacndOUkYR0_ozzT-vnyc&destination=${destinationCors}`);
 }
 
 
